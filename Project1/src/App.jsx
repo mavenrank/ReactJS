0bc084b9ffd468c6d7d8fd1fca3c5c58c1a271e2
mavenrank <Maverickodes.mac@gmail.com>
2024-01-21 11:32:44 +0530

    Adding Search Bar

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import { data } from "./data";
 import ProductTable from "./ProductTable"
+import SearchBar from "./SearchBar"
 function App() {
     return (
         <>
+            <SearchBar></SearchBar>
             <ProductTable data={data} />
         </>
     );
---
+++ Project1/src/SearchBar.jsx
@@ -0,0 +1,9 @@
+export default function SearchBar() {
+    return (
+        <>
+            <input type="text" placeholder="Search..." width="10%" ></input>
+            <button>Search</button>
+            <input type="checkbox">Show only in Stock</input>
+        </>
+    );
+}
