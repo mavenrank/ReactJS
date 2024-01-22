660be7257f9dc5e28720301e8969624c439e2e6a
mavenrank <Maverickodes.mac@gmail.com>
2024-01-22 07:16:43 +0530

    Recipes
    Iteration 1 Implementation

--- Project1/src/Products.jsx
+++ Project1/src/Products.jsx
@@ -1,15 +1,19 @@
-import ProductRow from "./ProductRow"
+import ProductRow from "./ProductRow";
 
 export default function Products({ data, category }) {
-    const products = []
+    const products = [];
 
-    data.forEach(item => {
-        products.push(<li><ProductRow product={item} /></li>)
-    })
+    data.forEach((item) => {
+        products.push(
+            <li>
+                <ProductRow product={item} />
+            </li>
+        );
+    });
     return (
         <>
             <h2>{category}</h2>
             {products}
         </>
-    )
+    );
 }
